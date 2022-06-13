072ca6328d6144a45cbf199ac5b4257d9e4f2a9c
EmilienKopp <emilien.kopp@gmail.com>
2022-06-13 16:19:46 +0900

    Basic app structure and boilerplate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { StudentComponent } from './student/student.component';
+import { ResultsComponent } from './results/results.component';
+import { ClassComponent } from './class/class.component';
+import { WorksheetComponent } from './worksheet/worksheet.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentComponent,
+    ResultsComponent,
+    ClassComponent,
+    WorksheetComponent
   ],
   imports: [
     BrowserModule
